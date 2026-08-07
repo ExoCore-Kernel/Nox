9277d3e055dff1c8aea494922779ed24a0720f6a
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 14:49:11 +0800

    Make logger uptime activation explicit

--- twilight/include/twilight/log.h
+++ twilight/include/twilight/log.h
@@ -3,5 +3,6 @@
 #include <stdint.h>
 
 void klog_init(void);
+void klog_enable_uptime(void);
 void klog(const char *message);
 void klog_color(const char *message, uint8_t red, uint8_t green, uint8_t blue);
