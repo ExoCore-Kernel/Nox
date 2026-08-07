f11a8d64f512c43651b5779810ab298e6a3b7d6d
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 17:24:38 +0800

    Expose heartbeat and console cursor APIs

--- twilight/include/twilight/log.h
+++ twilight/include/twilight/log.h
@@ -8,5 +8,6 @@ void klog_enable_uptime(void);
 void klog(const char *message);
 void klog_color(const char *message, uint8_t red, uint8_t green, uint8_t blue);
 void klog_parts(const char *const *parts, size_t count);
+void klog_heartbeat_enable(void);
 void klog_heartbeat_update(void);
 size_t klog_next_console_y(void);
