6363952586b680be60a1c0aa83b00c7c189d7ba1
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 22:51:14 +0800

    Add Linux 2.6 driver init annotations

--- twilight/include/linux/init.h
+++ twilight/include/linux/init.h
@@ -5,4 +5,9 @@
 #define __initdata
 #define __initconst
 #define __exitdata
+#define __devinit
+#define __devexit
+#define __devinitdata
+#define __devexitdata
+#define __devexit_p(fn) (fn)
 #define __maybe_unused __attribute__((unused))
