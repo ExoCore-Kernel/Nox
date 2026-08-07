675b0960598f9f0f534601692a367787089b124f
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 21:37:44 +0800

    Expose Linux driver runtime lifecycle

--- twilight/include/twilight/linux_compat.h
+++ twilight/include/twilight/linux_compat.h
@@ -4,3 +4,6 @@
 
 bool linux_compat_self_test(void);
 bool linux_pci_compat_self_test(void);
+bool linux_driver_runtime_init(void);
+bool linux_driver_runtime_self_test(void);
+void linux_driver_runtime_poll(void);
