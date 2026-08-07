2392ac426ceec3c697605e26a81535467d926760
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 14:32:54 +0800

    Add boot diagnostics and panic self-test

--- twilight/kernel/entry.c
+++ twilight/kernel/entry.c
@@ -6,6 +6,11 @@
 #include <twilight/framebuffer.h>
 #include <twilight/interrupts.h>
 #include <twilight/keyboard.h>
+#include <twilight/panic.h>
+
+#ifndef TWILIGHT_PANIC_SELF_TEST
+#define TWILIGHT_PANIC_SELF_TEST 0
+#endif
 
 __attribute__((used, section(".limine_requests_start")))
 static volatile uint64_t limine_requests_start[] = LIMINE_REQUESTS_START_MARKER;
@@ -34,6 +39,10 @@ static __attribute__((noreturn)) void fatal_halt(void) {
     halt_forever();
 }
 
+static void boot_status(const char *text, size_t line, uint8_t r, uint8_t g, uint8_t b) {
+    font_draw_string(text, 48, 48 + (font_height() + 2u) * line, r, g, b);
+}
+
 void kmain(void) {
     interrupts_disable();
 
@@ -57,16 +66,28 @@ void kmain(void) {
     }
 
     font_draw_string("Hello, World!", 48, 48, 235, 235, 235);
-    font_draw_string("PS/2 keyboard: type below", 48, 48 + font_height() + 2, 160, 200, 255);
+    boot_status("[1] framebuffer + font OK", 1, 150, 220, 150);
 
+#if TWILIGHT_PANIC_SELF_TEST
+    kernel_panic("Panic self-test: renderer is working");
+#endif
+
+    boot_status("[2] loading IDT...", 2, 180, 200, 255);
     idt_init();
+    boot_status("[3] IDT OK", 3, 150, 220, 150);
+
     pic_init();
+    boot_status("[4] PIC OK", 4, 150, 220, 150);
 
+    boot_status("[5] initializing PS/2 keyboard...", 5, 180, 200, 255);
     if (!ps2_keyboard_init()) {
-        font_draw_string("PS/2 keyboard init failed", 48, 48 + (font_height() + 2) * 2, 255, 120, 120);
-        fatal_halt();
+        kernel_panic("PS/2 keyboard initialization failed or timed out");
     }
+    boot_status("[6] PS/2 keyboard ACK received", 6, 150, 220, 150);
 
+    boot_status("[7] enabling interrupts...", 7, 180, 200, 255);
     interrupts_enable();
+    boot_status("[8] interrupts enabled - type below", 8, 150, 220, 150);
+
     halt_forever();
 }
