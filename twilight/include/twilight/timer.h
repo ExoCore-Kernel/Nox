00bca319366c16dd2558a687caf4cff2efde94fd
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 19:03:00 +0800

    Expose PIT counter diagnostics

--- twilight/include/twilight/timer.h
+++ twilight/include/twilight/timer.h
@@ -4,6 +4,7 @@
 
 void pit_init(uint32_t frequency_hz);
 void pit_irq_handler(void);
+uint16_t pit_read_counter(void);
 uint64_t timer_ticks(void);
 uint64_t timer_frequency(void);
 uint64_t timer_uptime_us(void);
