91e9427fd9daef20a3838a28cf39d0c9dfb76f48
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 14:44:04 +0800

    Add PIC mask helpers

--- twilight/include/twilight/interrupts.h
+++ twilight/include/twilight/interrupts.h
@@ -5,5 +5,7 @@
 void idt_init(void);
 void pic_init(void);
 void pic_send_eoi(uint8_t irq);
+void pic_mask_irq(uint8_t irq);
+void pic_unmask_irq(uint8_t irq);
 void interrupts_enable(void);
 void interrupts_disable(void);
