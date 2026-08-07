554364ca8511ab89ca3fb759f446043ad909cf8e
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 22:17:13 +0800

    Add minimal Linux sk_buff compatibility API

---
+++ twilight/include/linux/skbuff.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <linux/types.h>
+
+struct net_device;
+
+struct sk_buff {
+    u8 *head;
+    u8 *data;
+    u8 *tail;
+    u8 *end;
+    unsigned int len;
+    struct net_device *dev;
+    u16 protocol;
+};
+
+struct sk_buff *alloc_skb(unsigned int size, gfp_t flags);
+struct sk_buff *dev_alloc_skb(unsigned int size);
+struct sk_buff *netdev_alloc_skb(struct net_device *dev, unsigned int size);
+void dev_kfree_skb(struct sk_buff *skb);
+void kfree_skb(struct sk_buff *skb);
+
+u8 *skb_put(struct sk_buff *skb, unsigned int length);
+void skb_reserve(struct sk_buff *skb, unsigned int length);
+
+void skb_copy_and_csum_dev(const struct sk_buff *skb, void *destination);
