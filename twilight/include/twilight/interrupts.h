a720875db644c403ed462f5b6249dbc49a8e409d
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 19:02:28 +0800

    Add PIC and interrupt diagnostics

--- twilight/include/twilight/interrupts.h
+++ twilight/include/twilight/interrupts.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 
 void idt_init(void);
@@ -7,5 +8,9 @@ void pic_init(void);
 void pic_send_eoi(uint8_t irq);
 void pic_mask_irq(uint8_t irq);
 void pic_unmask_irq(uint8_t irq);
+uint8_t pic_master_mask(void);
+uint8_t pic_master_irr(void);
+uint8_t pic_master_isr(void);
 void interrupts_enable(void);
 void interrupts_disable(void);
+bool interrupts_are_enabled(void);
