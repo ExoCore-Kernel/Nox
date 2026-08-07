0650865a610c93bf48a75575c97d61ff10af0262
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 15:55:09 +0800

    Add framebuffer region scrolling API

--- twilight/include/twilight/framebuffer.h
+++ twilight/include/twilight/framebuffer.h
@@ -11,5 +11,7 @@ void framebuffer_clear(uint8_t red, uint8_t green, uint8_t blue);
 void framebuffer_put_pixel(size_t x, size_t y, uint8_t red, uint8_t green, uint8_t blue);
 void framebuffer_fill_rect(size_t x, size_t y, size_t width, size_t height,
                            uint8_t red, uint8_t green, uint8_t blue);
+void framebuffer_scroll_region_up(size_t top, size_t bottom, size_t pixels,
+                                  uint8_t red, uint8_t green, uint8_t blue);
 size_t framebuffer_width(void);
 size_t framebuffer_height(void);
