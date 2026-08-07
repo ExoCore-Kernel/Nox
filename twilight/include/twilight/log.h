4f161ba27c997fb6945c75a3aabd4d7355d10c27
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 16:04:31 +0800

    Add segmented kernel logging API

--- twilight/include/twilight/log.h
+++ twilight/include/twilight/log.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <stddef.h>
 #include <stdint.h>
 
 void klog_init(void);
 void klog_enable_uptime(void);
 void klog(const char *message);
 void klog_color(const char *message, uint8_t red, uint8_t green, uint8_t blue);
+void klog_parts(const char *const *parts, size_t count);
