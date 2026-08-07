87b4539d6d5887609dff9831b7a191b0974ada02
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 14:21:33 +0800

    Add framebuffer rectangle fill

--- twilight/include/twilight/framebuffer.h
+++ twilight/include/twilight/framebuffer.h
@@ -9,5 +9,7 @@ struct limine_framebuffer;
 bool framebuffer_init(struct limine_framebuffer *framebuffer);
 void framebuffer_clear(uint8_t red, uint8_t green, uint8_t blue);
 void framebuffer_put_pixel(size_t x, size_t y, uint8_t red, uint8_t green, uint8_t blue);
+void framebuffer_fill_rect(size_t x, size_t y, size_t width, size_t height,
+                           uint8_t red, uint8_t green, uint8_t blue);
 size_t framebuffer_width(void);
 size_t framebuffer_height(void);
