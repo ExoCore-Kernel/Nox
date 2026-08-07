1e920c980779be2aaeccc69287280b085cae43e0
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 21:39:59 +0800

    Expose Linux driver runtime initialized state

--- twilight/include/twilight/linux_compat.h
+++ twilight/include/twilight/linux_compat.h
@@ -5,5 +5,6 @@
 bool linux_compat_self_test(void);
 bool linux_pci_compat_self_test(void);
 bool linux_driver_runtime_init(void);
+bool linux_driver_runtime_is_initialized(void);
 bool linux_driver_runtime_self_test(void);
 void linux_driver_runtime_poll(void);
