7bcd4a28477a8e9f5f9afec065a917d0e2ecabf7
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 22:50:39 +0800

    Expand skb compatibility for upstream 8139too

--- twilight/include/linux/skbuff.h
+++ twilight/include/linux/skbuff.h
@@ -21,9 +21,14 @@ struct sk_buff *alloc_skb(unsigned int size, gfp_t flags);
 struct sk_buff *dev_alloc_skb(unsigned int size);
 struct sk_buff *netdev_alloc_skb(struct net_device *dev, unsigned int size);
 void dev_kfree_skb(struct sk_buff *skb);
+void dev_kfree_skb_any(struct sk_buff *skb);
 void kfree_skb(struct sk_buff *skb);
 
 u8 *skb_put(struct sk_buff *skb, unsigned int length);
 void skb_reserve(struct sk_buff *skb, unsigned int length);
-
+void skb_copy_to_linear_data(struct sk_buff *skb, const void *source, unsigned int length);
+void skb_copy_to_linear_data_offset(struct sk_buff *skb,
+                                    int offset,
+                                    const void *source,
+                                    unsigned int length);
 void skb_copy_and_csum_dev(const struct sk_buff *skb, void *destination);
