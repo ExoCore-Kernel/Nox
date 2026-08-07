7894cb7586638d6bd0398039610877202c9fad1d
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 21:35:44 +0800

    Add Linux delay compatibility helpers

---
+++ twilight/include/linux/delay.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <twilight/timer.h>
+
+static inline void msleep(unsigned int milliseconds) {
+    timer_sleep_ms(milliseconds);
+}
+
+static inline void mdelay(unsigned long milliseconds) {
+    timer_sleep_ms((uint64_t)milliseconds);
+}
+
+static inline void udelay(unsigned long microseconds) {
+    /* Sub-millisecond calibration will move to TSC/HPET later. This bounded
+     * pause loop is sufficient for current QEMU device register settling. */
+    volatile unsigned long iterations = microseconds * 128ul + 1ul;
+    while (iterations-- != 0ul) __asm__ volatile ("pause");
+}
+
+static inline void ndelay(unsigned long nanoseconds) {
+    udelay((nanoseconds + 999ul) / 1000ul);
+}
