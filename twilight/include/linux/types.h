c747762e873eff968f1e83eeed51a5fb85007875
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 22:51:44 +0800

    Add Linux endian scalar types

--- twilight/include/linux/types.h
+++ twilight/include/linux/types.h
@@ -22,7 +22,15 @@ typedef int16_t __s16;
 typedef int32_t __s32;
 typedef int64_t __s64;
 
+typedef u16 __le16;
+typedef u32 __le32;
+typedef u64 __le64;
+typedef u16 __be16;
+typedef u32 __be32;
+typedef u64 __be64;
+
 typedef uintptr_t phys_addr_t;
 typedef uint64_t dma_addr_t;
 typedef unsigned int gfp_t;
 typedef long ssize_t;
+typedef unsigned long ulong;
