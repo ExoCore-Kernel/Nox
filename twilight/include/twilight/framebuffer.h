f7f1321008e7cdbb66773e6f2b6692d6b04b681d
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 09:40:53 +0800

    Add framebuffer interface

---
+++ twilight/include/twilight/framebuffer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+struct limine_framebuffer;
+
+bool framebuffer_init(struct limine_framebuffer *framebuffer);
+void framebuffer_clear(uint8_t red, uint8_t green, uint8_t blue);
+void framebuffer_put_pixel(size_t x, size_t y, uint8_t red, uint8_t green, uint8_t blue);
+size_t framebuffer_width(void);
+size_t framebuffer_height(void);
