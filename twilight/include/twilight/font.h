d53081a81746f71e2793f4908876fdc41b595998
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 09:48:45 +0800

    Expose PSF font initialization

--- twilight/include/twilight/font.h
+++ twilight/include/twilight/font.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
+bool font_init(const void *data, size_t size);
 void font_draw_string(const char *text, size_t x, size_t y,
                       uint8_t red, uint8_t green, uint8_t blue);
