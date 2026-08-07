a51a09462087992843cdb27c28a19039dae052c5
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 20:30:21 +0800

    security: add x86 CPU hardening interface

---
+++ twilight/include/twilight/cpu_security.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdbool.h>
+
+struct cpu_security_status {
+    bool write_protect;
+    bool smep;
+    bool smap;
+    bool umip;
+};
+
+bool cpu_security_init(void);
+void cpu_security_get_status(struct cpu_security_status *out);
+
+/* Use these only around carefully validated kernel accesses to user pages. */
+void cpu_user_access_begin(void);
+void cpu_user_access_end(void);
