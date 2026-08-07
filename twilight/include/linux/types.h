296bc36689a1e629663522699e614c1effd31077
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 19:59:26 +0800

    compat: add Linux integer and address types

---
+++ twilight/include/linux/types.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef uint8_t __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
+typedef int8_t __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+typedef int64_t __s64;
+
+typedef uintptr_t phys_addr_t;
+typedef uint64_t dma_addr_t;
+typedef unsigned int gfp_t;
+typedef long ssize_t;
