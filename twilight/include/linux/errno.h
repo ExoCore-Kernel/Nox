a5bba4c507fd9496f3d151a8c5c3b82996d9cece
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 21:11:06 +0800

    linux-compat: add errno constants

---
+++ twilight/include/linux/errno.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define EPERM        1
+#define ENOENT       2
+#define EIO          5
+#define ENXIO        6
+#define ENOMEM      12
+#define EACCES      13
+#define EBUSY       16
+#define ENODEV      19
+#define EINVAL      22
+#define ENOSPC      28
+#define ENOSYS      38
+#define EOPNOTSUPP  95
