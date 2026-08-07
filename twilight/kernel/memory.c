f4091d7d28e73740b3f9caa949be839ccb50cfb8
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 15:36:30 +0800

    Prevent recursive optimization of memory primitives

--- twilight/kernel/memory.c
+++ twilight/kernel/memory.c
@@ -1,35 +1,59 @@
 #include <stddef.h>
 
+/*
+ * Freestanding memory primitives.
+ *
+ * The volatile accesses are deliberate: without them an optimizing compiler
+ * can recognize these loops and lower them back into calls to memset/memcpy,
+ * recursively calling the function we are currently implementing.
+ */
 void *memset(void *dest, int value, size_t count) {
-    unsigned char *d = (unsigned char *)dest;
-    for (size_t i = 0; i < count; ++i) d[i] = (unsigned char)value;
+    volatile unsigned char *d = (volatile unsigned char *)dest;
+    for (size_t i = 0; i < count; ++i) {
+        d[i] = (unsigned char)value;
+    }
     return dest;
 }
 
 void *memcpy(void *dest, const void *src, size_t count) {
-    unsigned char *d = (unsigned char *)dest;
-    const unsigned char *s = (const unsigned char *)src;
-    for (size_t i = 0; i < count; ++i) d[i] = s[i];
+    volatile unsigned char *d = (volatile unsigned char *)dest;
+    const volatile unsigned char *s = (const volatile unsigned char *)src;
+    for (size_t i = 0; i < count; ++i) {
+        d[i] = s[i];
+    }
     return dest;
 }
 
 void *memmove(void *dest, const void *src, size_t count) {
-    unsigned char *d = (unsigned char *)dest;
-    const unsigned char *s = (const unsigned char *)src;
-    if (d == s || count == 0) return dest;
-    if (d < s) {
-        for (size_t i = 0; i < count; ++i) d[i] = s[i];
-    } else {
-        for (size_t i = count; i > 0; --i) d[i - 1] = s[i - 1];
+    volatile unsigned char *d = (volatile unsigned char *)dest;
+    const volatile unsigned char *s = (const volatile unsigned char *)src;
+
+    if (d == s || count == 0) {
+        return dest;
     }
+
+    if ((const void *)d < (const void *)s) {
+        for (size_t i = 0; i < count; ++i) {
+            d[i] = s[i];
+        }
+    } else {
+        for (size_t i = count; i > 0; --i) {
+            d[i - 1] = s[i - 1];
+        }
+    }
+
     return dest;
 }
 
 int memcmp(const void *a, const void *b, size_t count) {
-    const unsigned char *aa = (const unsigned char *)a;
-    const unsigned char *bb = (const unsigned char *)b;
+    const volatile unsigned char *aa = (const volatile unsigned char *)a;
+    const volatile unsigned char *bb = (const volatile unsigned char *)b;
+
     for (size_t i = 0; i < count; ++i) {
-        if (aa[i] != bb[i]) return (int)aa[i] - (int)bb[i];
+        if (aa[i] != bb[i]) {
+            return (int)aa[i] - (int)bb[i];
+        }
     }
+
     return 0;
 }
