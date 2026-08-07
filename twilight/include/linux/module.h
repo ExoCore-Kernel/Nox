f1666f38a8e099a33bf50def2600cc5dde5a1a96
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-07 21:11:29 +0800

    linux-compat: add module metadata compatibility

---
+++ twilight/include/linux/module.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MODULE_LICENSE(value)
+#define MODULE_AUTHOR(value)
+#define MODULE_DESCRIPTION(value)
+#define MODULE_VERSION(value)
+#define MODULE_ALIAS(value)
+#define MODULE_DEVICE_TABLE(type, name)
+
+#define THIS_MODULE ((void *)0)
+
+#define __init
+#define __exit
+#define __maybe_unused __attribute__((unused))
