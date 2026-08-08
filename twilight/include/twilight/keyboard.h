7ffe9f3d8c62199d175448f290eb368e8329a308
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-08 15:15:50 +0800

    Expose framebuffer console output for TTY

--- twilight/include/twilight/keyboard.h
+++ twilight/include/twilight/keyboard.h
@@ -4,3 +4,7 @@
 
 bool ps2_keyboard_init(void);
 void keyboard_irq_handler(void);
+
+/* Shared early console sink used by the TTY so Linux userspace output appears
+ * on the framebuffer as well as COM1. */
+void keyboard_console_putc(char c);
