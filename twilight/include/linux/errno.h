0b68cde7642968164447cbe373a6788825b9a4a0
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-08 11:53:53 +0800

    Add errno values needed by libata

--- twilight/include/linux/errno.h
+++ twilight/include/linux/errno.h
@@ -4,6 +4,7 @@
 #define ENOENT       2
 #define EIO          5
 #define ENXIO        6
+#define EAGAIN      11
 #define ENOMEM      12
 #define EACCES      13
 #define EBUSY       16
@@ -12,3 +13,4 @@
 #define ENOSPC      28
 #define ENOSYS      38
 #define EOPNOTSUPP  95
+#define ETIMEDOUT  110
