3d17104ef8f66b5f1add28f7cbd277f999f21a55
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-08 14:19:48 +0800

    Expose Linux userspace ABI self-test

--- twilight/include/twilight/user.h
+++ twilight/include/twilight/user.h
@@ -5,6 +5,16 @@
 
 bool user_mode_is_available(void);
 bool user_mode_self_test(void);
+bool linux_user_self_test(void);
 
 /* Entry point used by the DPL3 int 0x80 assembly stub. */
 int user_syscall_dispatch(uint64_t syscall_number);
+
+/* Linux x86-64 syscall dispatcher used by the SYSCALL entry stub. */
+int64_t linux_syscall_dispatch(uint64_t syscall_number,
+                               uint64_t arg1,
+                               uint64_t arg2,
+                               uint64_t arg3,
+                               uint64_t arg4,
+                               uint64_t arg5,
+                               uint64_t arg6);
