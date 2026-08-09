284b89de18c263352f3ec125e3f759a2ea127b42
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-09 12:00:38 +0800

    Expose framebuffer metadata for Linux fbdev

--- twilight/include/twilight/framebuffer.h
+++ twilight/include/twilight/framebuffer.h
@@ -15,3 +15,13 @@ void framebuffer_scroll_region_up(size_t top, size_t bottom, size_t pixels,
                                   uint8_t red, uint8_t green, uint8_t blue);
 size_t framebuffer_width(void);
 size_t framebuffer_height(void);
+size_t framebuffer_pitch(void);
+size_t framebuffer_size(void);
+unsigned framebuffer_bpp(void);
+uint64_t framebuffer_physical_address(void);
+uint8_t framebuffer_red_mask_size(void);
+uint8_t framebuffer_red_mask_shift(void);
+uint8_t framebuffer_green_mask_size(void);
+uint8_t framebuffer_green_mask_shift(void);
+uint8_t framebuffer_blue_mask_size(void);
+uint8_t framebuffer_blue_mask_shift(void);
